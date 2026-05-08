bc2d75e69fcde69a58b2357acad9f48ae8c7c320
user <user@DESKTOP-349NBD4>
2026-05-08 16:38:26 +0900

    DispatcherTimer example 추가

---
+++ DispatcherTimer_example/DispatcherTimer_example/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace DispatcherTimer_example
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private DispatcherTimer timer;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer = new DispatcherTimer();
+
+            timer.Interval = TimeSpan.FromSeconds(1);
+
+            timer.Tick += Timer_Tick;
+
+            timer.Start();
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            ClockText.Text = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss");
+        }
+    }
+}
