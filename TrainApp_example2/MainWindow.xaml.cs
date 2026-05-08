57078a2d31f4d92f81136b0caea5da92febeb200
user <user@DESKTOP-349NBD4>
2026-05-08 18:53:39 +0900

    여러 열차가 있는 경우 구현

---
+++ TrainApp_example2/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TrainApp_example2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timer = new DispatcherTimer();
+
+        // 열차가 여러개일 경우, 열차 클래스를 만들고 리스트로 할당하는 것이 좋음
+        List<Train> trains = new List<Train>();
+
+        double x = 0;   // 열차 x축 위치
+        double y = 0;   // 열차 y축 위치
+        double speed = 5;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            CreateTrain(0, 50, 3);
+            CreateTrain(100, 100, 7);
+            CreateTrain(200, 150, 10);
+
+            // 0.1초마다 실행
+            timer.Interval = TimeSpan.FromMilliseconds(100);
+            timer.Tick += Train_Tick;
+            timer.Start();
+        }
+
+        private void CreateTrain(double x, double y, double speed)
+        {
+            Train t = new Train
+            {
+                X = x,
+                Y = y,
+                Speed = speed,
+                Shape = new Rectangle
+                {
+                    Width = 60,
+                    Height = 30,
+                    Fill = Brushes.Red
+                }
+            };
+
+            trains.Add(t);
+            MainCanvas.Children.Add(t.Shape);
+        }
+        private void Train_Tick(object sender, EventArgs e)
+        {
+            foreach (var t in trains)
+            {
+                t.X += t.Speed;
+
+                if (t.X > this.Width)
+                    t.X = -t.Shape.Width;
+
+                Canvas.SetLeft(t.Shape, t.X);
+                Canvas.SetTop(t.Shape, t.Y);
+            }
+        }
+
+    }
+}
---
+++ TrainApp_example2/Train.cs
@@ -0,0 +1,22 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace TrainApp_example2
+{
+    class Train
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Speed { get; set; }
+        public Rectangle Shape { get; set; }
+    }
+}
