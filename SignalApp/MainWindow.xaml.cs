a867f57e0190a4908600c27012444607cff850ec
user <user@DESKTOP-349NBD4>
2026-05-08 15:07:11 +0900

    초기 커밋.

---
+++ SignalApp/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SignalApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusText.Text = "운행중";
+
+            StatusText.Foreground = Brushes.Green;
+
+            SignalLamp.Fill = Brushes.Green;
+        }
+
+        private void StopButton_Click(object sender, RoutedEventArgs e)
+        {
+            StatusText.Text = "정지";
+
+            StatusText.Foreground = Brushes.Red;
+
+            SignalLamp.Fill = Brushes.Red;
+        }
+
+    }
+}
